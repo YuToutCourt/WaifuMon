17a296ffc3199872e8f33c800d7f8a1071fb25aa
Yù <wapzif@gmail.com>
2023-01-10 10:36:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ largest_screen = max(screens, key=lambda s: s[0] * s[1])
 screen_width, screen_height = largest_screen
 
 # Création de la fenêtre
-screen = pygame.display.set_mode((screen_width, screen_height),pygame.FULLSCREEN)
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 
 pygame.display.set_caption("WaifuMon")
 
