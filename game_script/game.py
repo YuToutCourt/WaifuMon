3808fac8c6340434de1d82c05ed6b30b3ac15115
Eragwen <gwendal_besnard@hotmail.com>
2023-06-21 11:00:16 +0200

    add new map and switch between both

--- game_script/game.py
+++ game_script/game.py
@@ -67,9 +67,61 @@ class Game:
 
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
+        enter_house = tmx_data.get_object_by_name("enter")
+        self.enter_house = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
         self.load_npc(tmx_data)
         self.dialog = Dialog()
         self.contact = False
+        self.map_name = "Desert"
+
+
+    def switch_map(self, map_name, spawn, direction):
+        tmx_data = pytmx.util_pygame.load_pygame(f"asset/{map_name}.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(
+            map_data, self.screen.get_size()
+        )
+
+        map_layer.zoom = 2
+
+        player_position = tmx_data.get_object_by_name(spawn)
+        coordinates = Coordinates(player_position.x, player_position.y)
+        self.player = Player(coordinates)
+
+        self.collisions = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.collisions.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        pygame.mixer.init()
+        pygame.mixer.fadeout(1000)
+
+        pygame.mixer.music.load("asset/music/main_theme.mp3")
+        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.play(-1)
+
+        self.direction_map = {
+            (pygame.K_UP, pygame.K_LEFT, pygame.K_RIGHT): "down",
+            (pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT): "up",
+            (pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN): "right",
+            (pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN): "left",
+            (pygame.K_UP, pygame.K_LEFT): "down_right",
+            (pygame.K_UP, pygame.K_RIGHT): "down_left",
+            (pygame.K_DOWN, pygame.K_LEFT): "up_right",
+            (pygame.K_DOWN, pygame.K_RIGHT): "up_left",
+            (pygame.K_UP,): "down",
+            (pygame.K_DOWN,): "up",
+            (pygame.K_LEFT,): "right",
+            (pygame.K_RIGHT,): "left",
+        }
+
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
+        self.load_npc(tmx_data)
+
+        enter_house = tmx_data.get_object_by_name(direction)
+        self.enter_house = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
 
     def load_npc(self, tmx_data):
         """
@@ -80,7 +132,7 @@ class Game:
             for obj in layer:
                 if not isinstance(obj, pytmx.TiledObject):
                     continue
-                if obj.name == "Player" or obj.name is None:
+                if obj.name == "Player" or obj.name == "enter" or obj.name == "spawn_outdoor" or obj.name=="exit" or obj.name == "spawn_indoor" or obj.name == "second_exit" or obj.name is None:
                     continue
                 coordinates = Coordinates(obj.x, obj.y)
                 npc = NPC(obj.name, coordinates, obj.properties["dialog"])
@@ -116,6 +168,13 @@ class Game:
                 ]:
                     self.contact = False
                     self.dialog.end_dialog()
+        if self.player.rect.colliderect(self.enter_house):
+            if self.map_name == "Desert":
+                self.switch_map("House", "spawn_indoor", "exit")
+                self.map_name = "House"
+            else:
+                self.switch_map("Desert", "spawn_outdoor", "enter")
+                self.map_name = "Desert"
                     
         for collision in self.collisions:
             if pygame.Rect.colliderect(self.player.rect, collision):
